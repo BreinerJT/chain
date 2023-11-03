c52377a9c54aace3aa489624f03694685f0c71bf
BreinerJT <breinerjtorres@gmail.com>
2023-11-03 17:19:32 -0500

    add landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,59 @@
-import { signIn, signOut, useSession } from "next-auth/react";
+import Link from 'next/link'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
-import { api } from "@/utils/api";
-import { Button } from "@/components/ui/button";
-
+import { buttonVariants } from '@/components/ui/button'
+import { Icons } from '@/components/Icons'
+import { LandingLayout } from '@/components/layouts/LandingLayout'
+import { api } from '@/utils/api'
 
 export default function Home() {
-
-  return (
-    <div>
-      <Button variant='outline'>Hola mundo</Button>
-    </div>
-  );
+	return (
+		<LandingLayout>
+			<div className='flex flex-col items-center justify-center	pb-20 pt-20'>
+				<h1 className='mb-2 text-3xl md:mb-5 md:text-6xl'>Chain</h1>
+				<h3 className='mb-2 text-2xl text-gray-400'>
+					Short URLs and custom slugs
+				</h3>
+				<h4 className='text-2xl text-gray-400'>Totally open source</h4>
+				<div className='mt-6 flex items-center'>
+					<Link href='/auth' className={buttonVariants({ variant: 'ghost' })}>
+						Get started
+					</Link>
+					<a
+						href='https://github.com/BreinerJT/chain'
+						target='_blank'
+						rel='noreferrer'
+						className={buttonVariants({ variant: 'ghost' })}
+					>
+						<Icons.star className='mr-2 h-4 w-4' />
+						Star on Github
+					</a>
+				</div>
+			</div>
+		</LandingLayout>
+	)
 }
 
 function AuthShowcase() {
-  const { data: sessionData } = useSession();
+	const { data: sessionData } = useSession()
 
-  const { data: secretMessage } = api.post.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
+	const { data: secretMessage } = api.post.getSecretMessage.useQuery(
+		undefined, // no input
+		{ enabled: sessionData?.user !== undefined }
+	)
 
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
+	return (
+		<div className='flex flex-col items-center justify-center gap-4'>
+			<p className='text-center text-2xl text-white'>
+				{sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+				{secretMessage && <span> - {secretMessage}</span>}
+			</p>
+			<button
+				className='rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20'
+				onClick={sessionData ? () => void signOut() : () => void signIn()}
+			>
+				{sessionData ? 'Sign out' : 'Sign in'}
+			</button>
+		</div>
+	)
 }
