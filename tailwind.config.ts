93dded6ddc5735ddee430aedefed0c982e1c0dcd
BreinerJT <breinerjtorres@gmail.com>
2023-11-03 17:19:14 -0500

    add container theme to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,21 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import { type Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 export default {
-  content: ["./src/**/*.tsx"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+	content: ['./src/**/*.tsx'],
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+			screens: {
+				'2xl': '1400px'
+			}
+		},
+		extend: {
+			fontFamily: {
+				sans: ['var(--font-sans)', ...fontFamily.sans]
+			}
+		}
+	},
+	plugins: []
+} satisfies Config
