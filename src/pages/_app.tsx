c77383197826d5622140cbd3bb629e851073ccd2
BreinerJT <breinerjtorres@gmail.com>
2023-11-03 17:20:30 -0500

    add app layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,31 @@
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
+import { Inter } from 'next/font/google'
+import { type Session } from 'next-auth'
+import { SessionProvider } from 'next-auth/react'
+import { type AppType } from 'next/app'
 
-import { api } from "@/utils/api";
+import { api } from '@/utils/api'
+import { cn } from '@/lib/utils'
 
-import "@/styles/globals.css";
+import '@/styles/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
+	Component,
+	pageProps: { session, ...pageProps }
 }) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
-};
+	return (
+		<SessionProvider session={session}>
+			<main
+				className={cn(
+					'min-h-screen bg-zinc-950 text-white antialiased',
+					inter.className
+				)}
+			>
+				<Component {...pageProps} />
+			</main>
+		</SessionProvider>
+	)
+}
 
-export default api.withTRPC(MyApp);
+export default api.withTRPC(MyApp)
