3a675d31dcbd9b708ee1e54991350c844093693f
BreinerJT <breinerjtorres@gmail.com>
2023-11-03 22:08:16 -0500

    fix navbar background style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { Authentication } from '@/components/Authentication'
 
 export const Navbar = () => {
 	return (
-		<header className='sticky top-0 z-40 w-full py-4'>
+		<header className='sticky top-0 z-40 w-full bg-[#121212] py-4'>
 			<div className='container mx-auto flex items-center justify-between pl-4 pr-4'>
 				<Link href='/'>
 					<div className='flex items-center transition-colors duration-300 hover:text-gray-300'>
