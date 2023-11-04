4cb30082ed3fb000f989adb4b3e20252877e98eb
BreinerJT <breinerjtorres@gmail.com>
2023-11-03 20:15:53 -0500

    remove container theme from tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,9 @@ import { type Config } from 'tailwindcss'
 import { fontFamily } from 'tailwindcss/defaultTheme'
 
 export default {
+	darkMode: ['class'],
 	content: ['./src/**/*.tsx'],
 	theme: {
-		container: {
-			center: true,
-			padding: '2rem',
-			screens: {
-				'2xl': '1400px'
-			}
-		},
 		extend: {
 			fontFamily: {
 				sans: ['var(--font-sans)', ...fontFamily.sans]
