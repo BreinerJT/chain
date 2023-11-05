6d7fa4aa852e652e99e1d2b86bf97d60adc8d567
BreinerJT <breinerjtorres@gmail.com>
2023-11-05 16:22:25 -0500

    add validations for new link form

--- src/pages/dashboard/new.tsx
+++ src/pages/dashboard/new.tsx
@@ -1,21 +1,72 @@
 import { GetServerSideProps } from 'next'
+import { useRouter } from 'next/navigation'
+
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 import { getServerAuthSession } from '@/server/auth'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Textarea } from '@/components/ui/textarea'
+import { CreateLinkSchema } from '@/schemas/link.schema'
+import { api } from '@/utils/api'
+import { useToast } from '@/components/ui/use-toast'
+
+type NewLinkForm = z.infer<typeof CreateLinkSchema>
 
 export default function NewLinkPage() {
+	const router = useRouter()
+	const { toast } = useToast()
+	const { register, handleSubmit, setError } = useForm<NewLinkForm>({
+		resolver: zodResolver(CreateLinkSchema)
+	})
+	const { mutate, isLoading } = api.link.create.useMutation({
+		onSuccess: () => {
+			router.push('/dashboard')
+			toast({
+				duration: 2000,
+				title: 'Looks good!',
+				description: 'Your link have been created successfully.'
+			})
+		},
+		onError: () => {
+			setError('slug', {
+				type: 'manual',
+				message: 'Slug already exist.'
+			})
+			toast({
+				title: 'Oops!',
+				description: 'Looks like the slug already exist, try with another one.',
+				variant: 'destructive'
+			})
+		}
+	})
+
+	const onSubmit = (data: NewLinkForm) => {
+		if (data.slug === data.url) {
+			setError('slug', {
+				message: 'Url and slug cannot be the same.',
+				type: 'manual'
+			})
+			return
+		}
+		mutate(data)
+	}
+
 	return (
-		<div className='container mx-auto mt-4 grid gap-8 px-4'>
+		<form
+			onSubmit={handleSubmit(onSubmit)}
+			className='container mx-auto mt-4 grid gap-8 px-4'
+		>
 			<div className='grid gap-4'>
 				<Label htmlFor='url'>URL</Label>
 				<Input
 					placeholder='https://'
 					className='dark:bg-zinc-900'
 					id='url'
-					name='url'
+					{...register('url')}
 				/>
 			</div>
 			<div className='grid gap-4'>
@@ -24,7 +75,7 @@ export default function NewLinkPage() {
 					placeholder='Custom slug'
 					className='dark:bg-zinc-900'
 					id='slug'
-					name='slug'
+					{...register('slug')}
 				/>
 			</div>
 			<div className='grid gap-4'>
@@ -32,13 +83,15 @@ export default function NewLinkPage() {
 				<Textarea
 					className='resize-none dark:bg-zinc-900'
 					id='description'
-					name='description'
+					{...register('description')}
 				/>
 			</div>
 			<div className='flex justify-end'>
-				<Button variant='ghost'>Create new Link</Button>
+				<Button disabled={isLoading} type='submit' variant='ghost'>
+					Create new Link
+				</Button>
 			</div>
-		</div>
+		</form>
 	)
 }
 
---
+++ src/schemas/link.schema.ts
@@ -0,0 +1,24 @@
+import z from 'zod'
+
+export const LinkSchema = z.object({
+	id: z.number(),
+	url: z.string(),
+	slug: z.string(),
+	description: z.string().optional()
+})
+
+export const CreateLinkSchema = z.object({
+	url: z.string().min(1, 'Url is required.').url('Enter a valid url.'),
+	slug: z
+		.string()
+		.min(1, 'Slug is required.')
+		.regex(
+			/^[a-zA-Z0-9_-]+$/i,
+			"Slugs can't have blank spaces or special characters."
+		),
+	description: z.string().optional()
+})
+
+export const GetLinkSchema = z.object({
+	id: z.number()
+})
