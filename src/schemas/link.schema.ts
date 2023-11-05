3bae9342d0cca8544a23d953ab3e551bc37d61b6
BreinerJT <breinerjtorres@gmail.com>
2023-11-05 17:46:30 -0500

    add schema to edit links

--- src/schemas/link.schema.ts
+++ src/schemas/link.schema.ts
@@ -19,6 +19,19 @@ export const CreateLinkSchema = z.object({
 	description: z.string().optional()
 })
 
+export const EditLinkSchema = z.object({
+	id: z.number(),
+	url: z.string().min(1, 'Url is required.').url('Enter a valid url.'),
+	slug: z
+		.string()
+		.min(1, 'Slug is required.')
+		.regex(
+			/^[a-zA-Z0-9_-]+$/i,
+			"Slugs can't have blank spaces or special characters."
+		),
+	description: z.string().optional()
+})
+
 export const GetLinkSchema = z.object({
 	id: z.number()
 })
