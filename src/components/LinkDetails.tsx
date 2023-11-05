0c261049b7b6693cc46620a9c241fd79f3f94bac
BreinerJT <breinerjtorres@gmail.com>
2023-11-05 17:47:23 -0500

    add edit dialog to link details

--- src/components/LinkDetails.tsx
+++ src/components/LinkDetails.tsx
@@ -1,17 +1,13 @@
 import { useState } from 'react'
 
-import { useToast } from '@/components/ui/use-toast'
-import { Icons } from '@/components/Icons'
 import { Button } from '@/components/ui/button'
-import { Label } from '@/components/ui/label'
-import { Input } from '@/components/ui/input'
+import { Dialog } from '@/components/ui/dialog'
+import { Icons } from '@/components/Icons'
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardHeader,
-	CardTitle
-} from '@/components/ui/card'
+	DeleteDialog,
+	EditDialog,
+	ShareDialog
+} from '@/components/LinkDetailsDialogs'
 import {
 	DropdownMenu,
 	DropdownMenuContent,
@@ -19,32 +15,18 @@ import {
 	DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
 import {
-	Dialog,
-	DialogClose,
-	DialogContent,
-	DialogDescription,
-	DialogFooter,
-	DialogHeader,
-	DialogTitle
-} from '@/components/ui/dialog'
-import { api } from '@/utils/api'
-import { copyToClipboard } from '@/utils/copyToClipboard'
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle
+} from '@/components/ui/card'
+import { Link } from '@/interfaces/link'
 
-interface Props {
-	url: string
-	slug: string
-	description: string
-	id: number
-}
-
-export const LinkDetails: React.FC<Props> = ({
-	description,
-	slug,
-	url,
-	id
-}) => {
-	const [isEditDialogOpen, setIsEditDialogOpen] = useState(false)
+export const LinkDetails = ({ description, slug, url, id }: Link) => {
 	const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false)
+	const [isEditDialogOpen, setIsEditDialogOpen] = useState(false)
+	const [isShareDialogOpen, setIsShareDialogOpen] = useState(false)
 
 	return (
 		<Card className='flex justify-between p-4 dark:bg-zinc-900'>
@@ -58,134 +40,47 @@ export const LinkDetails: React.FC<Props> = ({
 				<CardContent>{description}</CardContent>
 			</div>
 			<div className='py-6'>
-				<Dialog open={isEditDialogOpen} onOpenChange={setIsEditDialogOpen}>
-					<Dialog
-						open={isDeleteDialogOpen}
-						onOpenChange={setIsDeleteDialogOpen}
-					>
-						<DropdownMenu>
-							<DropdownMenuTrigger asChild>
-								<Button variant='ghost' size='sm'>
-									<Icons.moreVertical className='h-4 w-4' />
-									<span className='sr-only'>Link options</span>
-								</Button>
-							</DropdownMenuTrigger>
-							<DropdownMenuContent align='end' className='dark:bg-[#121212]'>
-								<DropdownMenuItem onClick={() => setIsEditDialogOpen(true)}>
-									<Icons.share className='mr-2 h-4 w-4' />
-									<span>Share</span>
-								</DropdownMenuItem>
-								<DropdownMenuItem>
-									<Icons.edit className='mr-2 h-4 w-4' />
-									<span>Edit</span>
-								</DropdownMenuItem>
-								<DropdownMenuItem onClick={() => setIsDeleteDialogOpen(true)}>
-									<Icons.trash className='mr-2 h-4 w-4' />
-									<span>Delete</span>
-								</DropdownMenuItem>
-							</DropdownMenuContent>
-						</DropdownMenu>
-						<DeleteDialog slug={slug} id={id} />
+				<Dialog open={isShareDialogOpen} onOpenChange={setIsShareDialogOpen}>
+					<Dialog open={isEditDialogOpen} onOpenChange={setIsEditDialogOpen}>
+						<Dialog
+							open={isDeleteDialogOpen}
+							onOpenChange={setIsDeleteDialogOpen}
+						>
+							<DropdownMenu>
+								<DropdownMenuTrigger asChild>
+									<Button variant='ghost' size='sm'>
+										<Icons.moreVertical className='h-4 w-4' />
+										<span className='sr-only'>Link options</span>
+									</Button>
+								</DropdownMenuTrigger>
+								<DropdownMenuContent align='end' className='dark:bg-[#121212]'>
+									<DropdownMenuItem onClick={() => setIsShareDialogOpen(true)}>
+										<Icons.share className='mr-2 h-4 w-4' />
+										<span>Share</span>
+									</DropdownMenuItem>
+									<DropdownMenuItem onClick={() => setIsEditDialogOpen(true)}>
+										<Icons.edit className='mr-2 h-4 w-4' />
+										<span>Edit</span>
+									</DropdownMenuItem>
+									<DropdownMenuItem onClick={() => setIsDeleteDialogOpen(true)}>
+										<Icons.trash className='mr-2 h-4 w-4' />
+										<span>Delete</span>
+									</DropdownMenuItem>
+								</DropdownMenuContent>
+							</DropdownMenu>
+							<DeleteDialog slug={slug} id={id} />
+						</Dialog>
+						<EditDialog
+							description={description}
+							id={id}
+							slug={slug}
+							url={url}
+							closeModal={() => setIsEditDialogOpen(false)}
+						/>
 					</Dialog>
 					<ShareDialog slug={slug} />
 				</Dialog>
 			</div>
 		</Card>
-	)
-}
-
-const ShareDialog = ({ slug }: { slug: string }) => {
-	const { toast } = useToast()
-
-	const onCopyToClipboard = async () => {
-		await copyToClipboard(`${window.location.origin}/r/${slug}`)
-		toast({
-			duration: 2000,
-			description: 'Link successfully copied.'
-		})
-	}
-
-	return (
-		<DialogContent className='sm:max-w-md'>
-			<DialogHeader>
-				<DialogTitle>Share link</DialogTitle>
-				<DialogDescription>
-					Anyone who has this link will be able to join your site.
-				</DialogDescription>
-			</DialogHeader>
-			<div className='flex items-center space-x-2'>
-				<div className='grid flex-1 gap-2'>
-					<Label htmlFor='link' className='sr-only'>
-						Link
-					</Label>
-					<Input
-						id='link'
-						defaultValue={`${window.location.origin}/r/${slug}`}
-						readOnly
-					/>
-				</div>
-				<Button
-					type='submit'
-					size='sm'
-					className='px-3'
-					onClick={onCopyToClipboard}
-				>
-					<span className='sr-only'>Copy</span>
-					<Icons.copy className='h-4 w-4' />
-				</Button>
-			</div>
-			<DialogFooter>
-				<DialogClose asChild>
-					<Button type='button' variant='secondary'>
-						Close
-					</Button>
-				</DialogClose>
-			</DialogFooter>
-		</DialogContent>
-	)
-}
-
-const DeleteDialog = ({ slug, id }: { slug: string; id: number }) => {
-	const { toast } = useToast()
-	const utils = api.useUtils()
-
-	const { mutate } = api.link.delete.useMutation({
-		onSuccess: data => {
-			utils.link.invalidate()
-			toast({
-				duration: 2000,
-				title: 'Success',
-				description: `${data.slug} have been deleted.`
-			})
-		},
-		onError: () => {
-			toast({
-				duration: 2000,
-				variant: 'destructive',
-				title: 'Uh oh! Something went wrong.',
-				description: 'There was a problem with your request.'
-			})
-		}
-	})
-
-	return (
-		<DialogContent>
-			<DialogHeader>
-				<DialogTitle>
-					Do you want to delete <code>/r/{slug}</code>?
-				</DialogTitle>
-				<DialogDescription>This action cannot be undone.</DialogDescription>
-			</DialogHeader>
-			<DialogFooter>
-				<Button onClick={() => mutate({ id })} type='button'>
-					Confirm
-				</Button>
-				<DialogClose asChild>
-					<Button type='button' variant='secondary'>
-						Close
-					</Button>
-				</DialogClose>
-			</DialogFooter>
-		</DialogContent>
 	)
 }
---
+++ src/components/LinkDetailsDialogs.tsx
@@ -0,0 +1,227 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Icons } from '@/components/Icons'
+import { Button } from '@/components/ui/button'
+import {
+	DialogHeader,
+	DialogFooter,
+	DialogContent,
+	DialogTitle,
+	DialogDescription,
+	DialogClose
+} from '@/components/ui/dialog'
+import { Textarea } from '@/components/ui/textarea'
+import { useToast } from '@/components/ui/use-toast'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+import { Link } from '@/interfaces/link'
+import { CreateLinkSchema } from '@/schemas/link.schema'
+import { api } from '@/utils/api'
+import { copyToClipboard } from '@/utils/copyToClipboard'
+
+export const ShareDialog = ({ slug }: { slug: string }) => {
+	const { toast } = useToast()
+
+	const onCopyToClipboard = async () => {
+		await copyToClipboard(`${window.location.origin}/r/${slug}`)
+		toast({
+			duration: 2000,
+			description: 'Link successfully copied.'
+		})
+	}
+
+	return (
+		<DialogContent className='sm:max-w-md'>
+			<DialogHeader>
+				<DialogTitle>Share link</DialogTitle>
+				<DialogDescription>
+					Anyone who has this link will be able to join your site.
+				</DialogDescription>
+			</DialogHeader>
+			<div className='flex items-center space-x-2'>
+				<div className='grid flex-1 gap-2'>
+					<Label htmlFor='link' className='sr-only'>
+						Link
+					</Label>
+					<Input
+						id='link'
+						defaultValue={`${window.location.origin}/r/${slug}`}
+						readOnly
+					/>
+				</div>
+				<Button
+					type='submit'
+					size='sm'
+					className='px-3'
+					onClick={onCopyToClipboard}
+				>
+					<span className='sr-only'>Copy</span>
+					<Icons.copy className='h-4 w-4' />
+				</Button>
+			</div>
+			<DialogFooter>
+				<DialogClose asChild>
+					<Button type='button' variant='secondary'>
+						Close
+					</Button>
+				</DialogClose>
+			</DialogFooter>
+		</DialogContent>
+	)
+}
+
+export const DeleteDialog = ({ slug, id }: { slug: string; id: number }) => {
+	const { toast } = useToast()
+	const utils = api.useUtils()
+
+	const { mutate } = api.link.delete.useMutation({
+		onSuccess: data => {
+			utils.link.invalidate()
+			toast({
+				duration: 2000,
+				title: 'Success',
+				description: `${data.slug} have been deleted.`
+			})
+		},
+		onError: () => {
+			toast({
+				duration: 2000,
+				variant: 'destructive',
+				title: 'Uh oh! Something went wrong.',
+				description: 'There was a problem with your request.'
+			})
+		}
+	})
+
+	return (
+		<DialogContent>
+			<DialogHeader>
+				<DialogTitle>
+					Do you want to delete <code>/r/{slug}</code>?
+				</DialogTitle>
+				<DialogDescription>This action cannot be undone.</DialogDescription>
+			</DialogHeader>
+			<DialogFooter>
+				<Button onClick={() => mutate({ id })} type='button'>
+					Confirm
+				</Button>
+				<DialogClose asChild>
+					<Button type='button' variant='secondary'>
+						Close
+					</Button>
+				</DialogClose>
+			</DialogFooter>
+		</DialogContent>
+	)
+}
+
+type EditLinkForm = z.infer<typeof CreateLinkSchema>
+interface EditDialogProps {
+	description: Link['description']
+	id: Link['id']
+	slug: Link['slug']
+	url: Link['url']
+	closeModal: () => void
+}
+
+export const EditDialog = ({
+	description,
+	id,
+	slug,
+	url,
+	closeModal
+}: EditDialogProps) => {
+	const { handleSubmit, register, setError } = useForm<EditLinkForm>({
+		defaultValues: {
+			description,
+			slug,
+			url
+		},
+		resolver: zodResolver(CreateLinkSchema)
+	})
+	const { toast } = useToast()
+	const utils = api.useUtils()
+
+	const { mutate } = api.link.update.useMutation({
+		onSuccess: () => {
+			utils.link.invalidate()
+			toast({
+				duration: 2000,
+				title: 'Looks good!',
+				description: `Your changes have been saved.`
+			})
+		},
+		onError: () => {
+			toast({
+				duration: 2000,
+				variant: 'destructive',
+				title: 'Uh oh! Something went wrong.',
+				description: 'There was a problem with your request.'
+			})
+		}
+	})
+
+	const onSubmit = (data: EditLinkForm) => {
+		if (data.slug === data.url) {
+			setError('slug', {
+				message: 'Url and slug cannot be the same.',
+				type: 'manual'
+			})
+			return
+		}
+
+		closeModal()
+
+		mutate({
+			...data,
+			id
+		})
+	}
+
+	return (
+		<DialogContent className='sm:max-w-[425px]'>
+			<DialogHeader>
+				<DialogTitle>Edit /r/{slug}</DialogTitle>
+				<DialogDescription>
+					Make changes to your link here. Click save when you're done.
+				</DialogDescription>
+			</DialogHeader>
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className='container mx-auto mt-4 grid gap-8 px-4'
+			>
+				<div className='grid gap-4'>
+					<Label htmlFor='url'>URL</Label>
+					<Input
+						placeholder='https://'
+						className='dark:bg-zinc-900'
+						id='url'
+						{...register('url')}
+					/>
+				</div>
+				<div className='grid gap-4'>
+					<Label htmlFor='slug'>Custom slug</Label>
+					<Input
+						placeholder='Custom slug'
+						className='dark:bg-zinc-900'
+						id='slug'
+						{...register('slug')}
+					/>
+				</div>
+				<div className='grid gap-4'>
+					<Label htmlFor='description'>Description (Optional)</Label>
+					<Textarea
+						className='resize-none dark:bg-zinc-900'
+						id='description'
+						{...register('description')}
+					/>
+				</div>
+				<DialogFooter>
+					<Button type='submit'>Save changes</Button>
+				</DialogFooter>
+			</form>
+		</DialogContent>
+	)
+}
