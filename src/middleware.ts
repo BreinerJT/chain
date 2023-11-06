dd2ad5a9b1b9aef7056f62b62d81dff58236933d
BreinerJT <breinerjtorres@gmail.com>
2023-11-06 13:41:35 -0500

    add slug api and middleware to redirect links to custom slugs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,38 @@
+import { NextResponse, type NextRequest } from 'next/server'
+
 import { getToken } from 'next-auth/jwt'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-	const isAuthPage = req.nextUrl.pathname.startsWith('/auth')
 	const session = await getToken({ req })
 
+	const isAuthPage = req.nextUrl.pathname.startsWith('/auth')
+
 	if (isAuthPage) {
-		if (!session) {
+		if (session) {
 			return NextResponse.redirect(new URL('/dashboard', req.url))
 		}
+	}
+
+	const isSlugPage = req.nextUrl.pathname.startsWith('/r')
+
+	if (isSlugPage) {
+		const slug = req.nextUrl.pathname.split('/').pop() as string
+		const data = await fetch(`${req.nextUrl.origin}/api/link/${slug}`)
+
+		const parsedData = await data.json()
+
+		if (data.status === 404) {
+			return NextResponse.redirect(
+				`${req.nextUrl.origin}/notFound?message=${parsedData.error}`
+			)
+		}
+
+		return NextResponse.redirect(new URL(parsedData.url))
 	}
 
 	return NextResponse.next()
 }
 
 export const config = {
-	matcher: ['/auth']
+	matcher: ['/auth', '/r/:slug*']
 }
