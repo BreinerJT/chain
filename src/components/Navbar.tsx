8e05f5bda2fa53dcca35ecb60af5889dfa5ccae8
BreinerJT <breinerjtorres@gmail.com>
2023-11-06 13:46:18 -0500

    fix styles in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,10 +6,10 @@ import { Authentication } from '@/components/Authentication'
 
 export const Navbar = () => {
 	return (
-		<header className='sticky top-0 z-40 w-full bg-[#121212] py-4'>
+		<header className='sticky top-0 z-40 w-full py-4 dark:bg-[#121212]'>
 			<div className='container mx-auto flex items-center justify-between pl-4 pr-4'>
 				<Link href='/'>
-					<div className='flex items-center transition-colors duration-300 hover:text-gray-300'>
+					<div className='flex items-center transition-colors duration-300 hover:text-gray-300 dark:hover:text-zinc-400'>
 						<Icons.logo />
 						<h2 className='ml-2 mr-2 text-xl'>Chain</h2>
 					</div>
