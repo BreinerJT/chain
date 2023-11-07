c010370f4b0c4513745e5182beabadf99e7fbe5c
BreinerJT <breinerjtorres@gmail.com>
2023-11-06 21:47:02 -0500

    add site config

---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+import { SiteConfig } from '@/types'
+
+export const siteConfig: SiteConfig = {
+	name: 'kadena',
+	description: 'URL shortener built with T3 Stack.',
+	url: 'https://kadena.vercel.app',
+	links: {
+		github: 'https://github.com/BreinerJT/kadena',
+		personal: 'https://github.com/BreinerJT'
+	}
+}
