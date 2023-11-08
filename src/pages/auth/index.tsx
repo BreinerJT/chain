e2035cfd7e45055c86162d658a036633be19a038
BreinerJT <breinerjtorres@gmail.com>
2023-11-08 14:36:22 -0500

    add loaders for login providers

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react'
 import { GetServerSideProps } from 'next'
+
 import { signIn } from 'next-auth/react'
 
 import { Icons } from '@/components/Icons'
@@ -6,20 +8,39 @@ import { Button } from '@/components/ui/button'
 import { getServerAuthSession } from '@/server/auth'
 
 export default function Auth() {
+	const [isGoogleLoading, setIsGoogleLoading] = useState(false)
+	const [isGitHubLoading, setIsGitHubLoading] = useState(false)
+
 	return (
 		<div className='container mx-auto'>
 			<div className='mt-16 grid items-center justify-center gap-8 px-4'>
 				<Button
 					variant='ghost'
-					onClick={() => signIn('google', { callbackUrl: '/dashboard' })}
+					onClick={() => {
+						setIsGoogleLoading(true)
+						signIn('google', { callbackUrl: '/dashboard' })
+					}}
 				>
-					<Icons.gmail className='mr-2 h-5 w-5' /> Sign in with Gmail
+					{isGoogleLoading ? (
+						<Icons.loader className='mr-2 h-5 w-5 animate-spin' />
+					) : (
+						<Icons.gmail className='mr-2 h-5 w-5' />
+					)}
+					Sign in with Gmail
 				</Button>
 				<Button
 					variant='ghost'
-					onClick={() => signIn('github', { callbackUrl: '/dashboard' })}
+					onClick={() => {
+						setIsGitHubLoading(true)
+						signIn('github', { callbackUrl: '/dashboard' })
+					}}
 				>
-					<Icons.github className='mr-2 h-5 w-5' /> Sign in with Github
+					{isGitHubLoading ? (
+						<Icons.loader className='mr-2 h-5 w-5 animate-spin' />
+					) : (
+						<Icons.github className='mr-2 h-5 w-5' />
+					)}
+					Sign in with Github
 				</Button>
 			</div>
 		</div>
